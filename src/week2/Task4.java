0ae8e6506adfec39c70cdfcd5a29add46868916e
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-02-08 18:13:45 +0530

    Initial commit

---
+++ src/week2/Task4.java
@@ -0,0 +1,19 @@
+package week2;
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number, reversed = 0;
+        System.out.println("Enter a number: ");
+        number = input.nextInt();
+        while(number!=0)
+        {
+            int remainder = number % 10;
+            reversed = reversed * 10 +remainder;
+            number /= 10;
+        }
+        System.out.println("The reversed number is: "+ reversed);
+
+    }
+}
